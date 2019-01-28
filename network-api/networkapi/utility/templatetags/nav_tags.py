ee5b53a6b789872deb57274c60b2faee3c6ac990
Mavis Ou <mmmavis@users.noreply.github.com>
2019-01-28 09:33:35 -0800

    temp work

--- network-api/networkapi/utility/templatetags/nav_tags.py
+++ network-api/networkapi/utility/templatetags/nav_tags.py
@@ -11,3 +11,14 @@ def horizontal_nav(context, current_page, menu_pages, classname=""):
         'menu_pages': menu_pages,
         'classname': classname,
     }
+
+
+# Instantiate a Buyer's Guide nav based on the current page's relation to other pages
+@register.inclusion_tag('tags/buyers_guide_nav.html', takes_context=True)
+def buyers_guide_nav(context, categories, category, current_path, classname=""):
+    return {
+        'categories': categories,
+        'category': category,
+        'classname': classname,
+        'current_path': current_path
+    }
